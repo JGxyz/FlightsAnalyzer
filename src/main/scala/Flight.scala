2f4451cb9591fbeaca0dffa4149ea818986ae79b
Jola Gluza <example@example.com>
2019-06-25 15:53:28 +0200

    removed unnecessary method from Flight

--- src/main/scala/Flight.scala
+++ src/main/scala/Flight.scala
@@ -1,5 +1,3 @@
 case class Flight(id: String,fldate: String,month:Integer, dofW: Integer, carrier: String, src: String,dst: String, crsdephour: Integer, crsdeptime: Integer, depdelay: Double, crsarrtime: Integer, arrdelay: Double, crselapsedtime: Double, dist: Double) {
-    def showSourceDest() = {
-      println("FLIGHT: "+id+"\nSOURCE: "+src+" DESTINATION: "+dst)
-    }
+
 }
